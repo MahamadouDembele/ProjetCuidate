08b6b97d5256c9adf80d73e72c564fa95a652596
Y4SS1N <115403954+Y4SS1N@users.noreply.github.com>
2022-12-21 18:24:12 +0100

    Add files via upload

---
+++ ProjetDW/Cuidate/insertTocard.php
@@ -0,0 +1,14 @@
+<?php
+include('../admin/config.php');
+if(isset($_POST['add'])){
+
+    $NAME = $_POST['name'];
+    $PRICE = $_POST['price'];
+
+    $insert = "INSERT INTO addcard (name, price) VALUES ('$NAME', '$PRICE')";
+    mysqli_query($con, $insert);
+    header('location:card.php');
+
+}
+
+?>
